61b91136a1563e1e276a3da8f2072d5543def564
Danielshdue <petrov.daniel0@gmail.com>
2025-11-06 09:15:19 +0200

    Add files via upload

---
+++ 16.10.25/16.10.25/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._10._25
+{
+    class Rectangle
+    {
+        private int height, width;
+        public void Input()
+        {
+            Console.WriteLine("Rectangle:");
+            Console.Write("Height= ");
+            height = int.Parse(Console.ReadLine());
+            Console.Write("Width= ");
+            width = int.Parse(Console.ReadLine());
+        }
+        public int Area()
+        {
+            return height * width;
+        }
+        public static int Parameter(Rectangle a)
+        {
+            return 2 * (a.width + a.height);
+        }
+        public void Output()
+        {
+            Console.WriteLine("Height and width of rectangle= {0}, {1}", +height, width);
+            Console.WriteLine("The area of the rectangle is {0}", +this.Area());
+            Console.WriteLine("The parameter of the rectangle is {0}", +Rectangle.Parameter(this));
+        }
+    }
+    class Triangle
+    {
+        private double a, b, c;
+        public void Input()
+        {
+            Console.WriteLine("Triangle:");
+            Console.Write("a= ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("a= ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("c= ");
+            c = int.Parse(Console.ReadLine());
+        }
+        public double Area()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        public static double Perimeter(Triangle tri)
+        {
+            return tri.a + tri.b + tri.c;
+        }
+        public void Output()
+        {
+            Console.WriteLine("The sides of the triangle are: {0}, {1}, {2}", +a, b, c);
+            Console.WriteLine("Area of triangle= {0}", +Area());
+            Console.WriteLine("Perimeter of triangle= {0}",+Triangle.Perimeter(this));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle();
+            rect1.Input();
+            rect1.Output();
+            Triangle tri = new Triangle();
+            tri.Input();
+            tri.Output();
+        }
+    }
+}
---
+++ 16.10.25/Zadacha2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha2
+{
+    class Point
+    {
+        private double x, y;
+        public string name;
+        public void Input()
+        {
+            Console.Write("x= ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("y= ");
+            y = double.Parse(Console.ReadLine());
+        }
+        public static void Input(Point a)
+        {
+            Console.WriteLine(a.name + " coordinates:");
+            Console.Write("x= ");
+            string tempx = Console.ReadLine();
+            a.x = int.Parse(tempx);
+            Console.Write("y= ");
+            string tempy = Console.ReadLine();
+            a.y = int.Parse(tempy);
+        }
+        public static void Output(Point a)
+        {
+            Console.WriteLine("The coordinates of "+a.name+" are: x= {0}, y= {1}", +a.x, a.y);
+        }
+        public static void Distance(Point a, Point b)
+        {
+            double dx = Math.Abs(a.x - b.x);
+            double dy = Math.Abs(a.y - b.y);
+            double razst = Math.Sqrt(dx * dx + dy * dy);
+            Console.WriteLine($"The distance between {a.name} and {b.name} is {razst:f2}");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point tochka1 = new Point();
+            tochka1.name = "tochka1";
+            Point tochka2 = new Point();
+            tochka2.name = "tochka2";
+            Point tochka3 = new Point();
+            tochka3.name = "tochka3";
+            Point.Input(tochka1);
+            Point.Input(tochka2);
+            Point.Input(tochka3);
+            Point.Distance(tochka1, tochka2);
+        }
+    }
+}
