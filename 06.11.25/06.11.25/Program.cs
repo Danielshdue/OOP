50ef44fd5781ec1b30fd7f77b6edafa978b9ee5c
Danielshdue <petrov.daniel0@gmail.com>
2025-11-06 09:45:48 +0200

    Add files via upload

---
+++ 06.11.25/06.11.25/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06._11._25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("a:");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("b:");
+                int b = int.Parse(Console.ReadLine());
+                int c = a / b;
+                Console.WriteLine($"c={c}");
+            }
+            catch (FormatException k)
+            {
+                Console.WriteLine(k.Message);
+            }
+            catch (OverflowException k)
+            {
+                Console.WriteLine(k.Message);
+            }
+            catch (DivideByZeroException k)
+            {
+                Console.WriteLine("Na 0 ne se deli!");
+            }
+            finally { Console.WriteLine("Goodbye"); }
+        }
+    }
+}
---
+++ 06.11.25/2ra zadacha/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ra_zadacha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("a: ");
+                int a = int.Parse(Console.ReadLine());
+                if (a <= 0)
+                {
+                    //throw new Exception("Nevalidna stojnost!");
+                    Exception Nevaliden = new Exception("Nevalidna stojnost!");
+                    throw Nevaliden;
+                }
+                double b = Math.Sqrt(a);
+                Console.WriteLine($"{b:f3}");
+            }
+            catch (Exception a)
+            {
+                Console.WriteLine(a.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of program.");
+            }
+        }
+    }
+}
