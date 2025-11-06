80f6f7d867f04f0d56bd1dcac31b4ce0283ff73f
Danielshdue <petrov.daniel0@gmail.com>
2025-11-06 09:18:13 +0200

    Add files via upload

---
+++ 30.10.25/2ra zadacha/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ra_zadacha
+{
+    class Car
+    {
+        private string brand, colour;
+        private int regnumber;
+        public Car(string brand, string colour, int number)
+        {
+            this.brand = brand;
+            this.colour = colour;
+            this.regnumber = number;
+        }
+        public Car()
+        {
+        }
+        public string Brand
+        {
+            get { return this.brand; }
+            set { brand = value; }
+        }
+        public string Colour
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+        public int RegNumber
+        {
+            get { return regnumber; }
+            set { regnumber = value; }
+        }
+        public void Info()
+        {
+            Console.WriteLine("The brand of the car is: "+this.Brand);
+            Console.WriteLine("The colour of the car is: " + this.Colour);
+            Console.WriteLine("The registration number of the car is: " + this.RegNumber);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car proba = new Car("Golf", "blue", 1523);
+            Car kola = new Car();
+            kola.Brand = Console.ReadLine();
+            kola.Colour = Console.ReadLine();
+            kola.RegNumber = int.Parse(Console.ReadLine());
+            proba.Info();
+            kola.Info();
+        }
+    }
+}
---
+++ 30.10.25/30.10.25/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30._10._25
+{
+    class Rectangle
+    {
+        private int height, length;
+        public Rectangle(int a, int b)
+        {
+            height = a;
+            length = b;
+        }
+        public Rectangle()
+        {
+            height = 0;
+            length = 0;
+        }
+        public int Height
+        {
+            set { height = value; }
+        }
+        public int Length
+        {
+            set { length = value; }
+        }
+        public int Area
+        {
+            get { return height * length; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle pravougulnik1 = new Rectangle(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            Rectangle pravougulnik2 = new Rectangle(13,7);
+            Rectangle pravougulnik3 = new Rectangle();
+            Rectangle[] masiv = new Rectangle[3];
+            masiv[0] = pravougulnik1;
+            masiv[1] = pravougulnik2;
+            masiv[2] = pravougulnik3;
+            Console.WriteLine("Pravougulnik3:");
+            Console.Write("Height: ");
+            pravougulnik3.Height = int.Parse(Console.ReadLine());
+            Console.Write("Length: ");
+            pravougulnik3.Length = int.Parse(Console.ReadLine());
+            Console.WriteLine("pravougulnik1's area is: " + pravougulnik1.Area);
+            Console.WriteLine("pravougulnik2's area is: " + pravougulnik2.Area);
+            Console.WriteLine("pravougulnik3's area is: " + pravougulnik3.Area);
+            int max =0;
+            for (int i = 0; i < masiv.Length; i++)
+            {
+                if (masiv[i].Area > max) max = masiv[i].Area;
+            }
+            Console.WriteLine("The largest area is " + max);
+
+            
+        }
+    }
+}
