ff98a95eb29e29555dca9cd4a38d2633cb043dd7
Danielshdue <petrov.daniel0@gmail.com>
2025-11-06 09:12:20 +0200

    Add files via upload

---
+++ 25.09.2025/25.09.2025/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._09._2025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of lines: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int space = n - i - 1; space > 0; space--)
+                {
+                    Console.Write(" ");
+                }
+                for (int lqvo = i; lqvo > 0; lqvo--)
+                {
+                    Console.Write("{0}",+lqvo);
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("{0}", +j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 25.09.2025/2razadacha/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2razadacha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inputs:");
+            int sum = 0;
+            int n;
+            do
+            {
+                n = int.Parse(Console.ReadLine());
+                sum += n;
+            } while (n != 0);
+            Console.WriteLine(sum);
+        }
+    }
+}
