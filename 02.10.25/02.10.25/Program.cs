412329433dec5ce4b5dbc7407571b693bd4c7280
Danielshdue <petrov.daniel0@gmail.com>
2025-11-06 09:14:07 +0200

    Add files via upload

---
+++ 02.10.25/02.10.25/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._10._25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of stations: ");
+            int broi = int.Parse(Console.ReadLine());
+            string[] stancii = new string[broi];
+            int[] temp = new int[broi];
+            for (int i = 0; i < stancii.Length; i++)
+            {
+                Console.Write("Station name: ");
+                stancii[i] = Console.ReadLine();
+                Console.Write("Measured temperature: ");
+                temp[i] = int.Parse(Console.ReadLine());
+            }
+            int mintemp=int.MaxValue;
+            foreach (int k in temp)
+            {
+                if (k < mintemp) mintemp = k;
+            }
+            int maxtemp = int.MinValue;
+            foreach (int k in temp)
+            {
+                if (k > maxtemp) maxtemp = k;
+            }
+            string[] minstancii=new string[broi];
+            int minindex = 0;
+            string[] maxstancii=new string[broi];
+            int maxindex = 0;
+            int maxLength = 0, minLength = 0;
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] == mintemp)
+                {
+                    minstancii[minindex] += stancii[i];
+                    minLength++;
+                    minindex++;
+                }
+                if (temp[i] == maxtemp)
+                {
+                    maxstancii[maxindex] += stancii[i];
+                    maxLength++;
+                    maxindex++;
+                }
+            }
+            if (minLength == 1)
+                Console.WriteLine("Lowest temperature: {0}°C, station: {1}", +mintemp, minstancii[0]);
+            else
+            {
+                Console.Write("Lowest temperature: {0}°C, stations: ", +mintemp);
+                for (int i = 0; i < minLength; i++)
+                {
+                    if(i!=minLength-1)
+                        Console.Write(minstancii[i]+", ");
+                    else
+                        Console.WriteLine(minstancii[i]);
+                }
+            }
+            if (maxLength == 1)
+                Console.WriteLine("Highest temperature: {0}°C, station: {1}", +maxtemp, maxstancii[0]);
+            else
+            {
+                Console.Write("Highest temperature: {0}°C, stations: ", +maxtemp);
+                for (int i = 0; i < maxLength; i++)
+                {
+                    if (i != maxLength - 1)
+                        Console.Write(maxstancii[i] + ", ");
+                    else
+                        Console.WriteLine(maxstancii[i]);
+                }
+            }
+            Console.WriteLine("Temperature difference: {0}°C",+(maxtemp-mintemp));
+        }
+    }
+}
---
+++ 02.10.25/masiv/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of elements: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n <= 10 || n > 50)
+            {
+                Console.WriteLine("Invalid size.");
+                return;
+            }
+            int[] input = new int[n];
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = int.Parse(Console.ReadLine());
+            }
+            int sbor = 0;
+            foreach (int k in input)
+            {
+                sbor += k;
+            }
+            Console.WriteLine("Srednoaritmetichnoto e {0}",+(sbor/n));
+            for (int i = 0; i < n; i += 2)
+            {
+                input[i] *= 15;
+            }
+            foreach (int k in input)
+            {
+                Console.Write(k+" "); 
+            }
+        }
+    }
+}
