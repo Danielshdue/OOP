03aaca6908e253ce7b68596f93f8f697d8b1cc7d
Danielshdue <petrov.daniel0@gmail.com>
2025-12-11 09:20:07 +0200

    Add files via upload

---
+++ 11.12.2025/11.12.2025/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._12._2025
+{
+    interface IFigure
+    {
+        void Input();
+        void Output();
+        void FindArea();
+    }
+    class Square:IFigure
+    {
+        private int side;
+        public void Input()
+        {
+            Console.Write("Square side= ");
+            side = int.Parse(Console.ReadLine());
+        }
+        public void Output()
+        {
+            Console.WriteLine($"The square has a side of {side}");
+        }
+        public void FindArea()
+        {
+            Console.WriteLine($"The square has an area of {side * side}");
+        }
+    }
+    class Circle : IFigure
+    {
+        private int radius;
+        public void Input()
+        {
+            Console.Write("Circle radius= ");
+            radius = int.Parse(Console.ReadLine());
+        }
+        public void Output()
+        {
+            Console.WriteLine($"The circle has a radius of {radius}");
+        }
+        public void FindArea()
+        {
+            Console.WriteLine($"The circle has an area of {(radius * radius * Math.PI):f2}");
+        }
+    }
+    class Triangle : IFigure
+    {
+        private int a,b,c;
+        public void Input()
+        {
+            Console.Write("Triangle A= ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Triangle B= ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Triangle C= ");
+            c = int.Parse(Console.ReadLine());
+        }
+        public void Output()
+        {
+            Console.WriteLine($"The triangle has sides {a}, {b} and {c}");
+        }
+        public void FindArea()
+        {
+            double p = (a + b + c) / 2;
+            Console.WriteLine($"The triangle has an area of {Math.Sqrt(p * (p - a) * (p - b) * (p - c)):f2}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFigure[] masiv = { new Square(), new Triangle(), new Circle() };
+            for (int i = 0; i < masiv.Length; i++)
+            {
+                masiv[i].Input();
+                masiv[i].Output();
+                masiv[i].FindArea();
+            }
+           
+        }
+    }
+}
---
+++ 11.12.2025/2ra zadacha/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ra_zadacha
+{
+    interface IPredator
+    {
+        void Walk();
+        void Run();
+        void Rest();
+        void Sound();
+    }
+    class Tiger : IPredator
+    {
+        void IPredator.Walk()
+        {
+            Console.WriteLine("The tiger walks silently in the jungle.");
+        }
+        void IPredator.Run()
+        {
+            Console.WriteLine("The tiger runs swiftly through the bushes to catch its prey.");
+        }
+        void IPredator.Rest()
+        {
+            Console.WriteLine("The tiger rests 10 hours a day.");
+        }
+        void IPredator.Sound()
+        {
+            Console.WriteLine("The tiger roars like a thunder.");
+        }
+    }
+    class Lion : IPredator
+    {
+        void IPredator.Walk()
+        {
+            Console.WriteLine("The lion sneaks through the tall grass.");
+        }
+        void IPredator.Run()
+        {
+            Console.WriteLine("The lion races to catch an antilope.");
+        }
+        void IPredator.Rest()
+        {
+            Console.WriteLine("The lion rests 17 hours a day.");
+        }
+        void IPredator.Sound()
+        {
+            Console.WriteLine("The lion roars like a mighty ruler.");
+        }
+    }
+    class Cat : IPredator
+    {
+        void IPredator.Walk()
+        {
+            Console.WriteLine("The cat walk elegantly around the living room.");
+        }
+        void IPredator.Run()
+        {
+            Console.WriteLine("The cat runs like a lightning.");
+        }
+        void IPredator.Rest()
+        {
+            Console.WriteLine("The cat rests 19 hours a day.");
+        }
+        void IPredator.Sound()
+        {
+            Console.WriteLine("The cat purrs softly.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat mycat = new Cat();
+            IPredator cat = mycat;
+            cat.Rest();
+            IPredator lion = new Lion();
+            lion.Run();
+            Tiger wildtiger = new Tiger();
+            IPredator tiger = wildtiger;
+            tiger.Sound();
+        }
+    }
+}
