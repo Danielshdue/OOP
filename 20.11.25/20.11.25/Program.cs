7884f4fb8cf28cdb4111d48d171b11d57de3714d
Danielshdue <petrov.daniel0@gmail.com>
2025-12-11 09:18:31 +0200

    Add files via upload

---
+++ 20.11.25/20.11.25/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._11._25
+{
+    class Pet
+    {
+        protected int age;
+        protected string name;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public virtual void Getsound()
+        {
+            Console.WriteLine("*animal sound*");
+        }
+        public virtual void Info()
+        {
+            Console.WriteLine($"My pet's name is {name}\nIts age is {age}");
+        }
+    }
+    class Cat : Pet
+    {
+        public Cat(string name, int age)
+        {
+            this.age = age;
+            this.name = name;
+        }
+        public override void Getsound()
+        {
+            Console.WriteLine("Meow meow");
+        }
+        public override void Info()
+        {
+            Console.WriteLine($"My cat's name is {name}\nIts age is {age}");
+        }
+    }
+    class Dog : Pet
+    {
+        public Dog(string name, int age)
+        {
+            this.age = age;
+            this.name = name;
+        }
+        public override void Getsound()
+        {
+            Console.WriteLine("Woof Woof");
+        }
+        public override void Info()
+        {
+            Console.WriteLine($"My dog's name is {name}\nIts age is {age}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet[] pets = { new Cat("Pisana",4), new Cat("Tom",3), new Dog("Sharo",1), new Dog("Johnny",5), new Dog("Bogi",2) };
+            foreach (Pet k in pets)
+            {
+                k.Getsound();
+                k.Info();
+            }
+        }
+    }
+}
---
+++ 20.11.25/2ra zadacha/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ra_zadacha
+{
+    class Dot2D
+    {
+        protected int x, y;
+        public Dot2D()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Dot2D(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public virtual void Info()
+        {
+            double distance = Math.Sqrt(x * x + y * y);
+            Console.WriteLine($"The dot's coordinates are:\nx={x}\ny={y}");
+            Console.WriteLine($"The dot's distance from point O is {distance}");
+        }
+    }
+    class Dot3D : Dot2D
+    {
+        private int z;
+        public Dot3D()
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+        public Dot3D(int x, int y, int z) : base(x, y)
+        {
+            //this.x = x;
+            //this.y = y;
+            this.z = z;
+        }
+        public override void Info()
+        {
+            double distance = Math.Sqrt(x * x + y * y + z * z);
+            Console.WriteLine($"The dot's coordinates are:\nx={x}\ny={y}\nz={z}");
+            Console.WriteLine($"The dot's distance from point O is {distance}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dot2D tochka;
+            Console.WriteLine("Press 2 for two-dimensional point.\nPress 3 for three-dimensional point.\nTo end the program type 'end'");
+            bool isit2or3 = false;
+            while (isit2or3==false)
+            {
+                try
+                {
+                    string  n = Console.ReadLine().ToLower();
+                    if (n != "2" && n != "3" && n!= "end")
+                    {
+                        throw new Exception("Invalid input. Choose between 2 and 3!");
+                    }
+                    switch (n)
+                    {
+                        case "2":
+                            Console.WriteLine("Enter x and y coordinates:");
+                            tochka = new Dot2D(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+                            tochka.Info();
+                            isit2or3 = true;
+                            break;
+                        case "3":
+                            Console.WriteLine("Enter x, y and z coordinates:");
+                            tochka = new Dot3D(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+                            tochka.Info();
+                            isit2or3 = true;
+                            break;
+                        case "end":
+                            isit2or3 = true;
+                            break;
+                    }
+                }
+                catch (Exception k)
+                {
+                    Console.WriteLine(k.Message);
+                    Console.WriteLine("Enter a number.");
+                }
+            }
+        }
+    }
+}
